60667e2fc50bba3f3d19af456b44475dce76ba6c
Dan Ko <danko56666@gmail.com>
2022-10-17 17:22:11 -0400

    Redux-Saga: Converting onAuthStateChanged Listener to Promise

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Checkout from "./routes/checkout/checkout.component";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
+  getCurrentUser,
 } from "./utils/firebase/firebase.utils";
 import { setCurrentUser } from "./store/user/user.action";
 
@@ -17,14 +18,7 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    getCurrentUser().then((user) => console.log(user));
   }, [dispatch]);
 
   return (
