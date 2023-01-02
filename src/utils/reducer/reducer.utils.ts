eda60c4d2ef7f6db793bb633af2e369547e339b0
Dan Ko <danko56666@gmail.com>
2023-01-02 18:37:14 -0500

    typing createAction and function overloading

---
+++ src/utils/reducer/reducer.utils.ts
@@ -0,0 +1,24 @@
+import { AnyAction } from "redux";
+
+export type ActionWithPayload<T, P> = {
+  type: T;
+  payload: P;
+};
+
+export type Action<T> = {
+  type: T;
+};
+
+export function createAction<T extends String, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
+
+export function createAction<T extends String>(
+  type: T,
+  payload: void
+): Action<T>;
+
+export function createAction<T extends String, P>(type: T, payload: P) {
+  return { type, payload };
+}
